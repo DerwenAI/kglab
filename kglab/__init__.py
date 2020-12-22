6c7ccef7d525c2f9e1bc166b434374217124f3f7
Paco Nathan <ceteri@gmail.com>
2020-12-22 15:28:37 -0800

    type checking

--- kglab/__init__.py
+++ kglab/__init__.py
@@ -1,3 +1,4 @@
-from .kglab import KnowledgeGraph, Subgraph, \
-    Measure, Simplex0, Simplex1, \
-    ShapeFactory, Leaderboard, EvoShape, EvoShapeNode, EvoShapeEdge
+from .kglab import KnowledgeGraph, Subgraph, RDF_Triple, SPARQL_Bindings, \
+    Measure, Simplex0, Simplex1, Census_Item, Census_Dyad_Tally, \
+    ShapeFactory, Leaderboard, EvoShape, EvoShapeNode, EvoShapeEdge, \
+    SerializedEvoShape, EvoShapeBoard, EvoShapeDistance
