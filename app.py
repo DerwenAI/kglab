9095258d98741d7252b07cdb8801a2442fe649f6
Paco Nathan <ceteri@gmail.com>
2020-12-27 19:31:26 -0800

    WIP online docs #32

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, redirect, render_template, send_from_directory, url_for
-from http import HTTPStatus
+from flask import Flask, redirect, send_from_directory, url_for
 from pathlib import PurePosixPath
 import os
-import sys
 
-
-######################################################################
-## app definitions
 
 APP = Flask(__name__, static_folder="site", template_folder="site")
 
@@ -19,20 +14,19 @@ APP.config["SECRET_KEY"] = "Technically, I remain uncommitted."
 APP.config["SEND_FILE_MAX_AGE_DEFAULT"] = 3000
 
 
-docs = os.path.join(os.path.dirname(os.path.abspath(__file__)), "site")
-
 @APP.route("/docs/", methods=["GET"])
 @APP.route("/docs/<path:path>", methods=["GET"], defaults={"path": None})
 @APP.route("/docs/<path:path>", methods=["GET"])
 def static_proxy (path=""):
-    #print("suffix", PurePosixPath(path).suffix)
+    if not path:
+        suffix = ""
+    else:
+        suffix = PurePosixPath(path).suffix
 
-    if path[-3:] not in ["css", ".js", "png"]:
+    if suffix not in [".css", ".js", ".png"]:
         path = os.path.join(path, "index.html")
 
-    return send_from_directory(docs, path)
-
-#return render_template(path, content=""), HTTPStatus.OK.value
+    return send_from_directory(APP.static_folder, path)
 
 
 @APP.route("/index.html")
@@ -42,13 +36,5 @@ def home_redirects ():
     return redirect(url_for("static_proxy"))
 
 
-######################################################################
-## main
-
-def main ():
-    PORT = 8000
-    APP.run(host="0.0.0.0", port=PORT, debug=True)
-
-
 if __name__ == "__main__":
-    main()
+    APP.run(host="0.0.0.0", port=8000, debug=True)
