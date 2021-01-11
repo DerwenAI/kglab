906660e3d89445cbf044d7e86dc8d87a55adb3d5
Paco Nathan <ceteri@gmail.com>
2021-01-11 14:05:56 -0800

    more apidocs and tests for serialization

--- test.py
+++ test.py
@@ -2,19 +2,56 @@
 # -*- coding: utf-8 -*-
 
 import kglab
+import os
+import pathlib
+import tempfile
 import urlpath
 import unittest
+import warnings
 
 
 class TestKG (unittest.TestCase):
-    def test_measure (self):
-        kg = kglab.KnowledgeGraph()
-        path = urlpath.URL("https://storage.googleapis.com/kglab-tutorial/foaf.rdf")
-        kg.load_rdf(path, format="xml")
+    def test_load_save_measure (self):
+        tmp = tempfile.NamedTemporaryFile(mode="w+b", delete=False)
 
-        measure = kglab.Measure()
-        measure.measure_graph(kg)
-        self.assertTrue(measure.node_count == 35)
+        try:
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore", message="unclosed file*")
+
+                # load RDF from urlpath.URL
+                kg = kglab.KnowledgeGraph()
+                path = urlpath.URL("https://storage.googleapis.com/kglab-tutorial/foaf.rdf")
+                kg.load_rdf(path, format="xml")
+                tmp.close()
+
+                # save RDF to local file reference
+                kg.save_rdf(tmp.name)
+                tmp.close()
+
+                # load RDF from pathlib.Path
+                kg = kglab.KnowledgeGraph()
+                path = pathlib.Path(tmp.name)
+                kg.load_rdf(path)
+                tmp.close()
+
+                # save JSON-LD to local file reference
+                kg.save_jsonld(tmp.name)
+                tmp.close()
+
+                # load JSON-LD from pathlib.Path
+                kg = kglab.KnowledgeGraph()
+                path = pathlib.Path(tmp.name)
+                kg.load_jsonld(path)
+
+                # measure graph
+                measure = kglab.Measure()
+                measure.measure_graph(kg)
+
+                # verify
+                self.assertTrue(measure.node_count == 35)
+        finally:
+            os.unlink(tmp.name)
+            tmp.close()
 
 
     def test_load_parquet_gs (self):
