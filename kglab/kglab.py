54d6987a59fb7c7f63c3b6367127d49f6e912da8
Paco Nathan <ceteri@gmail.com>
2021-01-18 15:38:58 -0800

    moved wiki info to mkdocs

--- kglab/kglab.py
+++ kglab/kglab.py
@@ -45,6 +45,7 @@ Core feature areas include:
     _DEFAULT_NAMESPACES: dict = {
         "dct":    "http://purl.org/dc/terms/",
         "owl":    "http://www.w3.org/2002/07/owl#",
+        "prov":   "http://www.w3.org/ns/prov#",
         "rdf":    "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
         "rdfs":   "http://www.w3.org/2000/01/rdf-schema#",
         "schema": "http://schema.org/",
