f8b95b38faaee72863adf08852945dc9f179fac2
Paco Nathan <ceteri@gmail.com>
2022-03-14 09:55:43 -0700

    updated libraries

--- scripts/graph.py
+++ scripts/graph.py
@@ -2,6 +2,7 @@
 from os.path import abspath, dirname
 import pathlib
 import sys
+import typing
 
 from icecream import ic
 from cryptography.hazmat.primitives import hashes  # type: ignore  # pylint: disable=E0401
@@ -29,15 +30,18 @@ if __name__ == "__main__":
         )
 
         lpg = PropertyStore.get_lpg(graph)
-        lpg.digest = hashes.Hash(hashes.BLAKE2b(64))  # type: ignore
+        ic(type(lpg))
 
+        lpg.digest = hashes.Hash(hashes.BLAKE2b(64))  # type: ignore
     else:
         graph = rdflib.Graph()
 
 
+    ## load RDF triples
+
     ttl_text = """
 @prefix : <http://www.w3.org/2012/12/rdf-val/SOTA-ex#> .
-@prefix foaf: <http://xmlns.com/foaf/0.1/'> .
+@prefix foaf: <http://xmlns.com/foaf/0.1/> .
 
 :peep0 a foaf:Person ;
     foaf:givenName "Alice" ;
@@ -62,18 +66,26 @@ if __name__ == "__main__":
     ic(len(graph))
 
 
+    ## run a SPARQL query
+
+    bindings: typing.Dict[ str, str ] = {
+        "surname": "Alice",
+    }
+
     sparql = """
     SELECT ?person ?pred ?surname
-    WHERE {
-        ?person ?pred ?surname
-    }
+      WHERE {
+        ?person ?pred ?surname .
+      }
     ORDER BY DESC(?surname)
     """
 
-    for row in graph.query(sparql):
+    for row in graph.query(sparql, initBindings=bindings):
         ic(row.asdict())
 
+
     sys.exit(0)
+    ## misc. CRUD operations
 
     s = rdflib.term.URIRef("https://www.food.com/recipe/327593")
     p = rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
