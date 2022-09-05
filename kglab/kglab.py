59bb4fc3b2aa6678589cd7960233de809d6a8387
Lorenzo (Mec-iS) <tunedconsulting@gmail.com>
2022-09-05 15:00:27 +0100

    Provide a graph factory method to be used by mixins to instantiate KGs

--- kglab/kglab.py
+++ kglab/kglab.py
@@ -383,3 +383,20 @@ must be a [`rdflib.term.Node`](https://rdflib.readthedocs.io/en/stable/apidocs/r
             ic(p)
             ic(o)
             raise TypeError(str(e))
+
+
+    def graph_factory(self, name, graph):
+            """
+Utility function to generate graphs from mixins
+
+        name:
+title of the report
+
+        graph:
+graph or subgraph to print the report for
+            """
+            return KnowledgeGraph(
+                name=name,
+                namespaces=self.get_ns_dict(),
+                import_graph=graph,
+            )
